2916a8b2d2b5d6a8005c438bc1d55f5c65e06ee1
chayandeep-yx <deepchayan0715@gmail.com>
2023-02-17 16:14:37 +0530

    jagaranweb

---
+++ shopping_db/admin/php_files/config.php
@@ -0,0 +1,8 @@
+<?php
+	include 'database.php';
+    //$base_url = "http://localhost/shoppingproject-yb/";
+    //  $base_url = "http://localhost/shopping-project/";
+     $base_url = "http://localhost/shopping_db";
+    
+
+?>
